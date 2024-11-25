0f8fd0ba59198d0a5e2ab66eac72d888600e87e6
Pepijn van Loenhout <pepijnvanloenhout@gmail.com>
2024-11-25 17:01:58 +0100

    Added endpoints for userservice for future implementation

--- apps/user-service/src/user.controller.ts
+++ apps/user-service/src/user.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from '../objects/user.interface';
+import { Friends } from '../objects/friends.interface';
 
 @Controller()
 export class UserController {
@@ -14,4 +16,34 @@ export class UserController {
     this.logger.log(`Fetched users: ${users}`);
     return users;
   }
+
+  @Get('/:id')
+  GetUserById(@Param('id') id : number ): string {
+    this.logger.log('Fetching user by id');
+    const user = this.userService.getUserById(id);
+    this.logger.log(`Fetched user: ${user}`);
+    return user;
+  }
+
+  @Get('/friends/:id')
+  GetFriends(@Param('id') id : number ): string {
+    this.logger.log('Fetching all friends of user');
+    const friends = this.userService.getFriends(id);
+    this.logger.log(`Fetched friends of user: ${friends}`);
+    return friends;
+  }
+
+  @Post('/add')
+  AddUser(@Body() user : User): void {
+    this.logger.log('Adding a new user');
+    this.userService.addUser(user);
+    this.logger.log(`Added user`);
+  }
+
+  @Post('/addFriend')
+  AddFriend(@Body() friends : Friends): void {
+    this.logger.log('Adding a new friend');
+    this.userService.addFriend(friends.userid1, friends.userid2);
+    this.logger.log(`Added friend`);
+    }
 }
